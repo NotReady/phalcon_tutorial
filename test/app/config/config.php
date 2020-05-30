0c110a7433763e3c89ccc72f0bd6cfdf8dc8366b
notready <loreley.lala@gmail.com>
2020-05-30 15:36:45 +0900

    voltのキャッシュファイルを別ディレクトリに変更 see https://phalcon-docs-ja.readthedocs.io/ja/stable/reference/volt.html

--- test/app/config/config.php
+++ test/app/config/config.php
@@ -6,10 +6,11 @@ return new Config(
         'application' => [
             'controllersDir' => '/app/controllers/',
             'modelsDir' => '/app/models/',
-            'viewsDir' => 'views',
+            'viewsDir' => '/app/views/',
             'formsDir' => '/app/forms/',
             'servicesDir' => '/app/services/',
             'helpersDir' => '/app/helpers/',
+            'cacheDir' => '/app/cache/',
         ]
     ]
 )
